e04492d7178779872384894d37e614b37cb3d142
Caramelo18 <fabiocaramelo18@gmail.com>
2016-04-06 17:19:53 +0100

    File comparison done

---
+++ rmdup.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <errno.h>
+
+#define FALSE 0
+#define TRUE 1
+#define BUF_LENGTH 256
+
+
+int hasSameContent(char name1[], char name2[])
+{
+  FILE *f1, *f2;
+  char buf[BUF_LENGTH], buf1[BUF_LENGTH];
+  if((f1 = fopen(name1, "r")) == NULL)
+  {
+    perror(name1);
+    printf("%d\n", errno);
+    exit(2);
+  }
+  if((f2 = fopen(name2 , "r")) == NULL)
+  {
+    perror(name2);
+    printf("%d\n", errno);
+    exit(2);
+  }
+
+  while((fgets(buf, BUF_LENGTH, f1) != NULL) && fgets(buf1, BUF_LENGTH, f2) != NULL)
+  {
+    if(strcmp(buf, buf1) != 0)
+      return FALSE;
+  }
+
+  return TRUE;
+}
+
+int isDup(char path1[], char file1[], char path2[], char file2[])
+{
+  char name1[30];
+  char name2[30];
+  struct stat stat1, stat2;
+
+/*  if(file1 != file2)
+  return FALSE;*/
+  sprintf(name1, "%s/%s", path1, file1);
+  sprintf(name2, "%s/%s", path2, file2);
+
+  printf("%s\n", name1);
+  printf("%s\n", name2);
+
+  stat(name1, &stat1);
+  stat(name2, &stat2);
+
+  if(stat1.st_mode != stat2.st_mode)
+    return FALSE;
+
+  return hasSameContent(name1, name2);
+}
+
+
+int main(int argc, char* argv[])
+{
+  DIR *dir;
+  if(argc != 2)
+  {
+    fprintf(stderr, "Usage: %s <dir_name> \n", argv[0]);
+    exit(1);
+  }
+
+  if((dir = opendir(argv[1])) == NULL)
+  {
+    perror(argv[1]);
+    exit(2);
+  }
+
+  if(isDup(argv[1], "f1", argv[1], "f2") == FALSE)
+  printf("FALSE\n");
+
+  closedir(dir);
+  return 0;
+}
